c8f391abf5f10fa1b443f924965403c04aa701bb
Axion741 <k.ballagher@gmail.com>
2018-08-16 17:50:38 +0100

    Added fetch request to get robot info externally, added scroll component to add scrollbox to around cards

---
+++ src/containers/app.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import CardList from '../components/cardList';
+import SearchBox from '../components/searchBox';
+import Scroll from '../components/scroll'
+import './app.css'
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+      
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        console.log(filteredRobots);  
+
+        if(this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+                return (
+                     <div className = 'tc'>
+                          <h1 className = 'f1'>RoboFriends</h1>
+                          <SearchBox searchChange={this.onSearchChange}/>
+                          <Scroll>
+                             <CardList robots={filteredRobots}/>    
+                          </Scroll>
+
+                      </div>
+                 ); 
+        }
+
+
+    }
+
+}
+
+export default App
